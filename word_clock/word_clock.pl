fa105135ae099735e43db488c26cd339a67438ad
Richard van Katwijk <richardvk@gmail.com>
2020-03-26 20:57:48 +1100

    Tidy up and fix some glaring issues (thanks to use strict)

--- word_clock/word_clock.pl
+++ word_clock/word_clock.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+use strict;
+use warnings;
 
-#$min=56;
-#$hour = 11;
+my $version = 20200326;
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 
 # Its just gonealmost ten
 # twenty quarterhalf past
@@ -19,21 +21,17 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 # six  eleven eight  nine 
 # ten seven twelve oclock
 
+my ($w1, $w2, $w3, $w4, $w5) = ('','','','','');
+
 my %words = (0=>'twelve',1=>'one',2=>'two',3=>'three',4=>'four',5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten',11=>'eleven',12=>'twelve',13=>'one',14=>'two',15=>'three',16=>'four',17=>'five',18=>'six',19=>'seven',20=>'eight',21=>'nine',22=>'ten',23=>'eleven',24=>'twelve');
 
+my $min_str = $min  < 10 ? '0'.$min  : $min;
+my $hr_str  = $hour < 10 ? '0'.$hour : $hour;
+
+# The start of all great things...
 my $sentence = "It's ";
 
-my $w1 = "";
-my $w2 = "";
-my $w2 = "";
-my $w2 = "";
-my $w2 = "";
-
-$min_str = $min<10 ?  '0'.$min  : $min;
-$hr_str  = $hour<10 ? '0'.$hour : $hour;
-
-###
-
+# Word one
 if (($min>=1 && $min<=5) || ($min>=11 && $min<=12) || ($min>=16 && $min<=17) || ($min>=21 && $min<=25) || ($min>=31 && $min<=35) || ($min>=41 && $min<=42) || ($min>=46 && $min<=47) || ($min>=51 && $min<=54)){
     $w1 = "just gone ";
 }
@@ -41,8 +39,7 @@ elsif (($min>=6 && $min<=9) || ($min>=11 && $min<=14) || ($min>=18 && $min<=19) 
     $w1 = "almost ";
 }
 
-###
-
+# Word two
 if (($min>=13 && $min<=17) || ($min>=43 && $min<=47)){
     $w2 = "quarter ";
 }
@@ -56,8 +53,8 @@ elsif (($min>=18 && $min<=25) || ($min>=36 && $min<=42)){
     $w2 = "twenty ";
 }
 
-###
 
+# Word three
 if (($min>=6 && $min<=35)) {
     $w3 = "past ";
 }
@@ -65,15 +62,16 @@ elsif (($min>=36 && $min<=54)) {
     $w3 = "to ";
 }
 
-###
-
+# Word for the hour
 my $word_hr = $hour;
+
 if ($min>=36 && $min<=59) {
     $word_hr = $hour+1;
 }
 
 $word_hr = $word_hr<=12 ? $word_hr : ($word_hr==24 ? 0 : $word_hr-12);
 
+# Word four
 if ($word_hr == 0 && ($min>=55 || $min<=5)) {
     $w4 = "midnight ";
 }
@@ -81,9 +79,10 @@ elsif ($word_hr == 12 && ($min>=55 || $min<=5)) {
     $w4 = "midday ";
 }
 else {
-    $w4 = $words{$word_hr}.' ';
+    $w4 = $words{$word_hr} . " ";
 }
 
+# Word five
 if ((($min>=0 && $min<=5) || ($min>=55 && $min<=59)) && $word_hr != 0 && $word_hr != 12) {
     $w5 = "o'clock";
 }
